4c690efcba7508785fba432c69555dc2bef7afd6
Lennard Behrens <behrenle@posteo.de>
2020-05-21 01:51:16 +0200

    fixed output module type

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ fs.readFile("./grammer.pegjs", "utf8", (err, data) => {
   if (err)
     throw err;
 
-  let parser = pegjs.generate(data, { output: "source" });
+  let parser = pegjs.generate(data, { output: "source", format: "commonjs" });
   fs.writeFile("./parser.js", parser, (err) => {
     if (err)
       throw err;
